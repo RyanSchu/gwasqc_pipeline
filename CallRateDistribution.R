3efdc1c264ed11a61d4e869301265e0bb7dc21a4
Ryan Schubert <31641912+RyanSchu@users.noreply.github.com>
2018-08-14 14:07:50 -0500

    Update CallRateDistribution.R

--- CallRateDistribution.R
+++ CallRateDistribution.R
@@ -4,35 +4,35 @@ library(ggplot2)
 library(argparse)
 
 parser <- ArgumentParser()
+parser$add_argument("-t", "--threshold", help="whatever threshold you filtered by", type="double", default=0.01 )
 parser$add_argument("--QCdir", help="directory where all the QC steps are written")
-parser$add_argument("-t", "--threshold", help="call rate filtering threshold. Keeps SNPs with call rates > 1-threshold.", type="double", default=0.01 )
 args <- parser$parse_args()
 
 "%&%" = function(a,b) paste (a,b,sep="")
-
-pdf(args$QCdir %&% "/QCplots/callRateDistributions.pdf")
 percentage<-100*(1-args$threshold)
 
-##Following QCStep1
-##displays the distribution of proportion of SNPs (F_MISS) missing in the sample
+#read in files
 lmiss <- read.table(args$QCdir %&% "/QCStep1/QCStep1.lmiss", header=T)
-hist(lmiss$F_MISS, main="Call rate distribution for SNPs before filtering")
-##Beginning SNP count
-dim(lmiss)[1]
-##SNPs with call rates > 99%
-table(lmiss$F_MISS<0.01)
-##percent SNPs with call rates > 99%
-table(lmiss$F_MISS<0.01)/sum(table(lmiss$F_MISS<0.01))
-
-##Following QCStep3
 imiss <- read.table(args$QCdir %&% "/QCStep3/QCStep3.imiss", header = T)
-hist(imiss$F_MISS, main="distributions for individuals after removing SNPs call rate < " %&% percentage)
-##Now plot SNPs with > 99% call rates
 newlmiss <- read.table(args$QCdir %&% "/QCStep3/QCStep3.lmiss", header = T)
+
+#create histogram's of missingness before and after filtering
+pdf(args$QCdir %&% "/QCstats/callRateDistributions.pdf")
+hist(lmiss$F_MISS, main="Call rate distribution for SNPs before filtering")
 hist(newlmiss$F_MISS, main="distributions for SNPs after removing SNPs call rate < " %&% percentage)
-
-##SNP and individual count after rm low-call SNPs
-dim(newlmiss)[1]
-dim(imiss)[1]
-
+hist(imiss$F_MISS, main="distributions for individuals after removing SNPs call rate < " %&% percentage)
 dev.off()
+
+#stats
+statsfile<- args$QCdir %&% "/QCstats/missingness.txt"
+#Initial SNP count, SNP count after filtering, individual count after filtering
+init<- "Initial number of SNPs is " "%&%" dim(lmiss)[1]
+final<- paste("Number of SNPs after filtering with out call rates < ", percentage, " is ", dim(newlmiss)[1], sep="")
+indiv<- "Number of individuals after filtering is " "%&%" dim(imiss)[1]
+write(init, statsfile, append=T)
+write(final, statsfile, append=T)
+write(indiv, statsfile, append=T)
+
+
+
+
