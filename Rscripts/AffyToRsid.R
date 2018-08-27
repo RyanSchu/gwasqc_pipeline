5388759f4461fbb06e4d9374e2c438f03d62daac
RyanSchu <rschubert1@luc.edu>
2018-08-27 12:55:14 -0500

    SNP ID translator script

---
+++ Rscripts/AffyToRsid.R
@@ -0,0 +1,19 @@
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(argparse)
+
+parser <- ArgumentParser()
+parser$add_argument("--bim", help="full path to the bim file you would like to translate")
+parser$add_argument("--csv",help="full path to file containing the Affy SNPs with corresponding rsids")
+parser$add_argument("--QCdir", help="directory where all the QC steps are written")
+args <- parser$parse_args(c('--csv', 'Z:/test.csv', '--QCdir', 'Z:/QC', '--bim', 'Z:/test.bim'))
+
+"%&%" = function(a,b) paste (a,b,sep="")
+
+csv<-read.table(args$csv, header = T, skip = 18, sep = ',')
+bim<-read.table(args$bim, sep = '\t')
+bim<- left_join(bim, csv, by = c('V2' = 'Probe.Set.ID'))
+newbim<- select(bim, c('V1', 'dbSNP.RS.ID', 'V3', 'V4', 'V5', 'V6' ))
+
+write.table(x= newbim, file = args$bim, quote = F, row.names = F,col.names = F)
