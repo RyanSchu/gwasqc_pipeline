0ac488597d0a8fff82b6514da4478907d65ac85e
RyanSchu <rschubert1@luc.edu>
2018-08-30 16:05:17 -0500

    changed argument flags for flexibility

--- Rscripts/ibd.R
+++ Rscripts/ibd.R
@@ -4,16 +4,17 @@ library(ggplot2)
 library(argparse)
 
 parser <- ArgumentParser()
-parser$add_argument("--pihat", help="inbreeding coefficient to filter by")
-parser$add_argument("--QCdir", help="directory where all the QC steps are written")
+parser$add_argument("-g", "--genome", help="Full path to the .genome file you wish to use")
+parser$add_argument("-o", "--outputdir", help="directory where you would like to output your plots")
+parser$add_argument("-p", "--pihat", help="inbreeding coefficient to filter by")
 args <- parser$parse_args()
 
 "%&%" = function(a,b) paste (a,b,sep="")
 
 ##Following QCStep5b
-ibd <- read.table(args$QCdir %&% "/QCStep5/QCStep5b/QCstep5b.genome", header = T)
-pdf(args$QCdir %&% "/QCstats/IBD.pdf")
-ggplot(data=ibd,aes(x=Z0,y=Z1))+geom_point(alpha=1/4)+theme_bw()
+ibd <- read.table(args$genome, header = T)
+png(args$outputdir %&% "/IBD.png")
+ggplot(data=ibd,aes(x=Z0,y=Z1))+geom_point(alpha=1/4)+theme_bw() + xlim(0,1)
 dev.off()
 ##pull duplicates
 dups <- data.frame()
@@ -30,11 +31,11 @@ a <- as.character(ibd$IID1) %in% toExclude
 others <- ibd[a==FALSE,]
 dim(others)
 
-pdf(args$QCdir %&% "/QCstats/ibd_pi_hat.pdf")
+pdf(args$outputdir %&% "/ibd_pi_hat.pdf")
 hist(others$PI_HAT)
 dev.off()
 
 #sortOthers <- others[order(others$PI_HAT, decreasing = TRUE),]
 ##Unexpected duplicates:
 #filter(others,PI_HAT>=0.2)
-write.table(filter(others,PI_HAT>=args$pihat), args$QCdir %&% "/QCstats/related.to.remove.txt", quote = FALSE, row.names = FALSE)
+write.table(filter(others,PI_HAT>=args$pihat), args$outputdir %&% "/related.to.remove.txt", quote = FALSE, row.names = FALSE)
