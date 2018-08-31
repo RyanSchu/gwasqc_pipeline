48158f10afe0e14e4e58234b90569d02e87dbc2a
RyanSchu <rschubert1@luc.edu>
2018-08-31 13:14:35 -0500

    fixed argumetn flags for flexibility

--- Rscripts/heterozygosity.R
+++ Rscripts/heterozygosity.R
@@ -4,19 +4,21 @@ library(ggplot2)
 library(argparse)
 
 parser <- ArgumentParser()
+parser$add_argument("--unfilteredhet", help="full path to the unfiltered het file")
+parser$add_argument("--filteredhet", help="full path to the filtered het file")
 parser$add_argument("-r","--relatedness",help="Threshold of relatedness used for filtering")
-parser$add_argument("--QCdir", help="directory where all the QC steps are written")
+parser$add_argument("-o", "--outputdir", help="directory where you would like to output your plots")
 args <- parser$parse_args()
 
 "%&%" = function(a,b) paste (a,b,sep="")
 
-hetstats<-args$QCdir %&% "/QCstats/Hetstats.txt"
-HET <- read.table(args$QCdir %&% "/QCStep5/QCStep5c/QCStep5c.het", header = T, as.is = T)
-newHET <- read.table(args$QCdir %&% "/QCStep5/QCStep5e/QCStep5e.het", header = T, as.is = T)
+hetstats<-args$outputdir %&% "/Hetstats.txt"
+HET <- read.table(args$unfilteredhet, header = T, as.is = T)
+newHET <- read.table(args$filteredhet, header = T, as.is = T)
 H = (HET$N.NM.-HET$O.HOM.)/HET$N.NM.
 oldpar = par(mfrow=c(1,2))
 
-pdf(args$QCdir %&% "/QCstats/Heterozygosity.pdf")
+pdf(args$outputdir %&% "/Heterozygosity.pdf")
 hist(H,50, main="H estimate before filtering")
 hist(HET$F,50, main="Heterozygosity estimates prior to filtering")
 abline(v=mean(HET$F)+6*sd(HET$F),col="red")
@@ -45,7 +47,7 @@ for(i in 1:length(sortHET$F)){
 hetoutliers <- select(outliers, FID, IID)
 dim(hetoutliers)
 allexclude2 <- hetoutliers
-write.table(allexclude2, file = args$QCdir %&% "/QCStep5/QCStep5c/QCStep5c.txt", quote = F, col.names = F, row.names = F)
+write.table(allexclude2, file = args$outputdir %&% "/03het_unfiltered.txt", quote = F, col.names = F, row.names = F)
 
 
 newsortHET <- newHET[order(newHET$F),]
@@ -62,6 +64,6 @@ for(i in 1:length(newsortHET$F)){
 newhetoutliers <- select(outliers, FID, IID)
 dim(newhetoutliers)
 newallexclude2 <- newhetoutliers
-write.table(newallexclude2, file = args$QCdir %&% "/QCStep5/QCStep5e/QCStep5e.txt", quote = F, col.names = F, row.names = F)
+write.table(newallexclude2, file = paste(args$outputdir, "/05het_without_relateds_", args$relatedness,".txt", sep=""), quote = F, col.names = F, row.names = F)
 count <- paste(dim(newhetoutliers)[1], "to remove after filtering")
 write(count, hetstats, append=T)
